163c01d277ba46883a71f7c19dd9eb29281b0996
chinmaykoshe <141853866+chinmaykoshe@users.noreply.github.com>
2025-10-02 09:00:17 +0530

    CHAT APP REMOVED

--- CHINMAY/java1/App.java
+++
@@ -1,7 +0,0 @@
-package java1;
-
-public class App{
-    public static void Sum(){
-        System.out.println("hii im sum");
-    }
-}
--- CHINMAY/java2/NewApp.java
+++
@@ -1,9 +0,0 @@
-package java2;
-
-import java1.App;
-
-public class NewApp {
-    public static void main(String[]args){
-        App.Sum();
-    }
-}
